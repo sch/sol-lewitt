162de8f8df07d4fea8c557758663ad693d963288
Adrian Schaedle <adrian@schaedle.me>
2017-04-02 12:58:15 -0400

    Add support for remote figwheel repls

--- project.clj
+++ project.clj
@@ -7,12 +7,16 @@
  :dependencies [[org.clojure/clojure "1.8.0"]
                 [org.clojure/clojurescript "1.9.494"]
                 [org.clojure/core.async "0.3.442"]
-                [rum "0.10.8"]]
+                [rum "0.10.8"]
+                [figwheel-sidecar "0.5.8"]
+                [com.cemerick/piggieback "0.2.1"]]
 
  :plugins [[lein-cljsbuild "1.1.5"]
            [lein-figwheel "0.5.9"]]
 
  :source-paths ["src"]
+
+ :repl-options {:nrepl-middleware [cemerick/piggieback/wrap-cljs-repl]}
 
  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
@@ -34,34 +38,33 @@
                                   :optimizations :advanced
                                   :pretty-print false}}]}
 
- :figwheel {
-             ;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
+ :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+            ;; :server-port 3449 ;; default
+            ;; :server-ip "127.0.0.1"
 
-            :css-dirs ["resources/public/css"]}) ;; watch and update CSS
+            :css-dirs ["resources/public/css"] ;; watch and update CSS
 
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
+            ;; Start an nREPL server into the running figwheel process
+            :nrepl-port 7888})
 
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
+            ;; Server Ring Handler (optional)
+            ;; if you want to embed a ring handler into the figwheel http-kit
+            ;; server, this is for simple ring servers, if this
+            ;; doesn't work for you just run your own server :)
+            ;; :ring-handler hello_world.server/handler
 
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
+            ;; To be able to open files in your editor from the heads up display
+            ;; you will need to put a script on your path.
+            ;; that script will have to take a file path and a line number
+            ;; ie. in  ~/bin/myfile-opener
+            ;; #! /bin/sh
+            ;; emacsclient -n +$2 $1
+            ;;
+            ;; :open-file-command "myfile-opener"
 
-             ;; if you want to disable the REPL
-             ;; :repl false
+            ;; if you want to disable the REPL
+            ;; :repl false
 
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+            ;; to configure a different figwheel logfile path
+            ;; :server-logfile "tmp/logs/figwheel-logfile.log"
 
